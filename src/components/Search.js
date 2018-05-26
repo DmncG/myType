2e061341526ebdf70f64b7f877c90757e766890c
DmncG <drbgarcia389@yahoo.com>
2018-05-26 01:05:13 -0400

    feat search autosuggest functional

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,12 +3,60 @@ import {Navbar} from '../components'
 import TextField from 'material-ui/TextField'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import Autosuggest from 'react-autosuggest'
+
 
 class Search extends Component {
   constructor (props) {
     super(props)
-    this.state = {value: 'Search for your font'}
+    this.state = {value: '', suggestions: []}
     this.handleChange = this.handleChange.bind(this)
+    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this)
+    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this)
+    this.getSuggestionvalue = this.getSuggestionValue.bind(this)
+    this.renderSuggestion = this.renderSuggestion.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onSuggestionsFetchRequested (value) {
+    let getSuggestions = (value) => {
+      console.log({value})
+      const languages = this.props.fontList.items.map(font => font.family)
+      const inputVal = value.trim().toLowerCase()
+      const inputLength = inputVal.length
+      console.log({languages})
+      return inputLength === 0 ? [] : languages.filter(lang => {
+        return lang.toLowerCase().slice(0, inputLength) === inputVal
+      })
+    }
+    this.setState({
+      suggestions: getSuggestions(value.value)
+    })
+  }
+
+  renderSuggestion (suggestion) {
+    return (
+      <div>
+        {suggestion}
+      </div>
+    )
+  }
+
+  getSuggestionValue (suggestion) {
+    return suggestion
+  }
+
+  onSuggestionsClearRequested () {
+    this.setState({
+      suggestions: []
+    })
+  }
+
+  onChange (event, { newValue }) {
+    this.setState({
+      value: newValue
+    })
+    event.preventDefault()
   }
 
   handleChange (e) {
@@ -17,44 +65,59 @@ class Search extends Component {
   }
   render () {
     const {fontList} = this.props
+    const {value, suggestions} = this.state
+
+    const inputProps = {
+      placeholder: 'Search for a font',
+      value,
+      onChange: this.onChange
+    }
+
     return (
       <div>
         <Navbar/>
-        {
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={this.getSuggestionValue}
+          renderSuggestion={this.renderSuggestion}
+          inputProps={inputProps}
+          theme={{padding: '100px'}}
+        />
+        {/* {
           fontList.items &&
           <div className='search-content'>
             <TextField
               onChange={this.handleChange}
-              value={this.state.value}
+              value={value}
               underlineStyle={{borderColor: '#000000'}}
               id='search-textfield'/>
             <p> Search Results</p>
             <ul style={{listStyleType: 'none'}} className="directory-ul">
-              {
-                fontList.items.filter(font => {
+              {this.state.suggestions.length
+                ? fontList.items.filter(font => {
                   if (font.family === this.state.value) {
                     return font
                   }
                 }).map(font => {
                   return (
-                    <li key={font.family} className="directory-li" style={{fontFamily: `${font.family}`}}>
+                    <li key={font.family} className="directory-li">
                       <Link to={`/font/${font.family}`} className="directory-link">{font.family}</Link>
                     </li>
                   )
-
-                  // if (font.family.match(/{`${this.state.value}`}/gi)) {
-                  //   console.log('matches')
-                  //   return (
-                  //     <li key={font.family} className="directory-li" style={{fontFamily: `${font.family}`}}>
-                  //       <Link to={`/font/${font.family}`} className="directory-link">{font.family}</Link>
-                  //     </li>
-                  //   )
-                  // }
+                })
+                : fontList.items.map(font => {
+                  return (
+                    <li key={font.family} className="directory-li">
+                      <Link to={`/font/${font.family}`} className="directory-link">{font.family}</Link>
+                    </li>
+                  )
                 })
               }
             </ul>
           </div>
-        }
+        } */}
       </div>
     )
   }
